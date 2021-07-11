e0d9e4607e6f8af9a753e243bce73ec70ad12324
hyrodium <hyrodium@gmail.com>
2021-07-11 23:45:34 +0900

    update language list

--- src/HyrodiumHome.jl
+++ src/HyrodiumHome.jl
@@ -28,4 +28,36 @@ using Documenter
 #     cd(repo_dir)
 # end
 
+
+"""
+Generate language list
+"""
+function Documenter.generate_version_file(versionfile::AbstractString, _entries, symlinks = [])
+    entries = ["en", "ja"]
+    open(versionfile, "w") do buf
+        println(buf, "var DOC_VERSIONS = [")
+        for folder in entries
+            println(buf, "  \"", folder, "\",")
+        end
+        println(buf, "];")
+
+        # entries is empty if no versions have been built at all
+        isempty(entries) && return
+
+        # The first element in entries corresponds to the latest version, but is usually not the full version
+        # number. So this essentially follows the symlinks that will be generated to figure out the full
+        # version number (stored in DOCUMENTER_CURRENT_VERSION in siteinfo.js).
+        # Every symlink points to a directory, so this doesn't need to be recursive.
+        newest = first(entries)
+        for s in symlinks
+            if s.first == newest
+                newest = s.second
+                break
+            end
+        end
+        println(buf, "var DOCUMENTER_NEWEST = \"$(newest)\";")
+        println(buf, "var DOCUMENTER_STABLE = \"$(first(entries))\";")
+    end
+end
+
 end
