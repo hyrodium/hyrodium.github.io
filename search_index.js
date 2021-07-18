24f6fe12d2d9fd37190b5749c51586aae66b008c
Documenter.jl <documenter@juliadocs.github.io>
2021-07-18 17:20:19 +0000

    build based on 5e54563

--- search_index.js
+++ search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#","page":"404","title":"404","text":"","category":"section"},{"location":"","page":"404","title":"404","text":"Select a language from the side menu and find your way.\nサイドメニューから言語を選択してどうぞ。","category":"page"}]
+[{"location":"#","page":"404","title":"404","text":"","category":"section"},{"location":"","page":"404","title":"404","text":"English\n日本語","category":"page"}]
 }
