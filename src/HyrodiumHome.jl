f5454a3649c93dca974e84658ba70e2b855a6d36
hyrodium <hyrodium@gmail.com>
2021-07-21 22:46:37 +0900

    update license

--- src/HyrodiumHome.jl
+++ src/HyrodiumHome.jl
@@ -4,7 +4,7 @@ using Documenter
 
 """
 Generate language list
-The following script is a modified version of the Documenter.jl script. (See LICENSE)
+The following script is a modified version of the Documenter.jl script.
 """
 function Documenter.Writers.HTMLWriter.generate_version_file(versionfile::AbstractString, entries, symlinks = [])
     script = """
@@ -16,15 +16,6 @@ function Documenter.Writers.HTMLWriter.generate_version_file(versionfile::Abstra
     var DOCUMENTER_STABLE = "en";
     """
     write(versionfile, script)
-
-    # open(versionfile, "w") do buf
-    #     println(buf, "var DOC_VERSIONS = [")
-    #     println(buf, "  \"en\",")
-    #     println(buf, "  \"ja\",")
-    #     println(buf, "];")
-    #     println(buf, "var DOCUMENTER_NEWEST = \"en\";")
-    #     println(buf, "var DOCUMENTER_STABLE = \"en\";")
-    # end
 end
 
 end
